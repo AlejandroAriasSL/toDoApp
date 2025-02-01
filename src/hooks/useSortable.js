70015036b8450072fd2e77329b38c8f6c5ff8f14
AlejandroAriasSL <alejandroasilva02@gmail.com>
2025-02-01 16:04:05 +0100

    fixed drag-n-drop so it can auto-scroll

--- src/hooks/useSortable.js
+++ src/hooks/useSortable.js
@@ -1,10 +1,19 @@
 import { useEffect} from "react";
 import Sortable from "sortablejs";
 
+
 export const useSortable = (listRef, onEndCallBack) => {
     useEffect(() => {
         if (listRef.current){
-            const sortable = Sortable.create(listRef.current, {
+            const sortable = Sortable.create(listRef.current, 
+                {
+                scroll: true,
+                scrollSensitivity: 20,
+                scrollSpeed: 2,
+                animation: 800,
+                easing: "cubic-bezier(0.65, 0, 0.35, 1)",
+                forceFallback: true,
+                fallbackOnBody: false,
                 onEnd(e) {
                     if(onEndCallBack) {
                         onEndCallBack(e.oldIndex, e.newIndex);
